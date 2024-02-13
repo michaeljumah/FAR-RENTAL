14ba3c7b28bbae717e87b389d531264bfd6e7e6d
michaeljumah <michaeljumah8@gmail.com>
2024-02-14 02:06:43 +0300

    i finnished my project

---
+++ rental/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rental.db'
+app.config['SECRET_KEY'] = '38686699db8a350e76a19df4'
+db = SQLAlchemy(app)
+app.app_context().push()
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from rental import routes
