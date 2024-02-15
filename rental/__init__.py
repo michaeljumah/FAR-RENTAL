64eac0d1b8b09d5a369994f4c83cd5b52078f0bf
michaeljumah <michaeljumah8@gmail.com>
2024-02-15 11:37:58 +0300

    configured the database

--- rental/__init__.py
+++ rental/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rental.db'
 app.config['SECRET_KEY'] = '38686699db8a350e76a19df4'
