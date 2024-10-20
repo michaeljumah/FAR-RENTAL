a1a44063b843554239cec31fec6e569ad03bf156
michaeljumah <michaeljumah8@gmail.com>
2024-10-21 02:43:53 +0300

    sqlalchemy problem

--- rental/__init__.py
+++ rental/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://farentaldb_1n03_user:emk3TijQADx3qjRDzNXKim3Nj8rzSdQh@dpg-csanp608fa8c73cquepg-a.oregon-postgres.render.com/farentaldb_1n03') #'sqlite:///rental.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#app.config['SECRET_KEY'] = '38686699db8a350e76a19df4'
+app.config['SECRET_KEY'] = '38686699db8a350e76a19df4'
 db = SQLAlchemy(app)
 app.app_context().push()
 bcrypt = Bcrypt(app)
