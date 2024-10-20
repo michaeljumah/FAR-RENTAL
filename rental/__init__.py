18f72994ba755f7056921426c6895227ae6ee376
michaeljumah <michaeljumah8@gmail.com>
2024-10-21 01:03:57 +0300

    moving to postgresql

--- rental/__init__.py
+++ rental/__init__.py
@@ -6,6 +6,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rental.db'
+
+#postgresql://farentaldb_1n03_user:emk3TijQADx3qjRDzNXKim3Nj8rzSdQh@dpg-csanp608fa8c73cquepg-a.oregon-postgres.render.com/farentaldb_1n03
 app.config['SECRET_KEY'] = '38686699db8a350e76a19df4'
 db = SQLAlchemy(app)
 app.app_context().push()
