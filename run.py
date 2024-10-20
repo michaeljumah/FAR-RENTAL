09fe488adfe47612ef8df94b0e1a4fe025bd3361
michaeljumah <michaeljumah8@gmail.com>
2024-10-21 02:38:01 +0300

    sqlalchemy problem

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from rental import app
 
 #Checks if the run.py file has executed directly and not imported........
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
