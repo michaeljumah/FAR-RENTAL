bd7e572118fd74bab4059c2116987443af0b11cd
michaeljumah <michaeljumah8@gmail.com>
2025-05-05 15:06:25 +0300

    linked the navigation bar with the neccessary codes part

--- rental/__init__.py
+++ rental/__init__.py
@@ -6,7 +6,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://farentaldb_1n03_user:emk3TijQADx3qjRDzNXKim3Nj8rzSdQh@dpg-csanp608fa8c73cquepg-a.oregon-postgres.render.com/farentaldb_1n03') #'sqlite:///rental.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://farentaldb_1n03_user:emk3TijQADx3qjRDzNXKim3Nj8rzSdQh@dpg-csanp608fa8c73cquepg-a.oregon-postgres.render.com/farentaldb_1n03') #'sqlite:///rental.db replaced'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SECRET_KEY'] = '38686699db8a350e76a19df4'
